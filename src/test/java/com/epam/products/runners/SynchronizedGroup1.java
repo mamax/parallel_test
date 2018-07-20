d85062d7fd826500b86b9a2e4cf747e61157148f
Andrii <marchenkoandy@gmail.com>
2018-07-21 00:08:12 +0300

    fix

--- src/test/java/com/epam/products/runners/SynchronizedGroup1.java
+++ src/test/java/com/epam/products/runners/SynchronizedGroup1.java
@@ -5,11 +5,10 @@ import cucumber.api.testng.AbstractTestNGCucumberTests;
 
 @CucumberOptions(
         strict = true,
-        features = "classpath:products/cucumber/",
+        features = "classpath:cucumber/",
         glue = "com.epam.products.tests",
         tags = {"@SynchronizedGroup1", "~@Ignore"},
         plugin = {"pretty",
-                "com.epam.reportportal.cucumber.StepReporter",
                 "json:target/cucumber-results/SynchronizedGroup1.json",
                 "rerun:target/cucumber-results/SynchronizedGroup1.txt"})
 public class SynchronizedGroup1 extends AbstractTestNGCucumberTests {
--- src/test/resources/cucumber/Feature1.feature
+++ src/test/resources/cucumber/Feature1.feature
@@ -3,8 +3,8 @@ Feature: Test Feature 1 for parallel purpose
 
   Background:
     Given BackGround step sleep "5" sec
-    And BackGround step sleep "5" sec
 
+  @SynchronizedGroup1
   Scenario Outline: Test Scenario 1 in TestFeature 1
     Given Step with sleep "<sleep_sec>" sec in Scenario "<scenario_number>" of TestFeature "<feature_number>"
     Examples:
